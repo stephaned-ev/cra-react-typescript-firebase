59fad37ffe56dbb3c283b17f03d02023c2d001af
stephaned-ev <stephaned@protonmail.ch>
2020-08-09 19:56:46 +0200

    Initial commit: CRA, TypeScript template, ESLint and prettier configured.

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    // parser: 'babel-eslint',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  plugins: [],
+  // add your custom rules here
+  rules: {
+    'react/prop-types': 1,
+  },
+};
